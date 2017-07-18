20cce0c1d4fb429524921160d0b6d172dc718e50
emily-li <emily.li@live.co.uk>
2017-07-18 23:22:47 +0100

    Adding simple Spring Boot project

---
+++ src/main/java/com/liemily/web/WebApplication.java
@@ -0,0 +1,16 @@
+package com.liemily.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * WebApplication Spring Boot starter
+ * Created by Emily Li on 16/07/2017.
+ */
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
